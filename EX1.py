03cc6da7b68a46ec7557af7880897c576b847cec
JoseAVillalba <145340615+JoseAVillalba@users.noreply.github.com>
2024-02-02 10:09:34 +0100

    Update EX1.py

--- EX1.py
+++ EX1.py
@@ -10,3 +10,15 @@ resultado_final = list(zip(res[0], res[1]))
 
 
 print(set(resultado_final))
+.................................................
+taula=[[0]*15 for i in range(5)]
+
+for i in range(5):
+    for j in range(15):
+        if i == 0 or i == 4 or j == 0 or j == 14:
+            taula[i][j]=1
+
+for fila in taula:
+    for elemento in fila:
+        print(elemento, end='')
+    print()
